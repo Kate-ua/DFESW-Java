9f9f2939b2ec9aeeb7dbedc0279878c8e05e7bcd
Kate-ua <lesnaya_k@hotmail.com>
2022-06-14 10:14:40 +0100

    KM: initial commit

---
+++ src/com/qa/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.qa.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+    }
+	public static void printMessage() {
+		// DataType variableName = value; 
+		String message = "Hellow World";
+		
+		// println is used to print by line;
+		System.out.println(message);
+		
+//		print is used to print the text to the same line  
+	}
+}
