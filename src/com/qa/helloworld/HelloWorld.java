eb5ccf6ed858533e55f54aa47f4f94b2daedac42
Kate-ua <lesnaya_k@hotmail.com>
2022-06-14 12:10:08 +0100

    updating

--- src/com/qa/helloworld/HelloWorld.java
+++ src/com/qa/helloworld/HelloWorld.java
@@ -3,15 +3,20 @@ package com.qa.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
+		String result = returnString ("Original value");
+		System.out.println(result);
     }
-	public static void printMessage() {
+	public static void printMessage(String myMessage) {
 		// DataType variableName = value; 
 		String message = "Hellow World";
 		
 		// println is used to print by line;
-		System.out.println(message);
+		System.out.println(myMessage);
 		
 //		print is used to print the text to the same line  
 	}
-}
+
+    public static String returnString (String myMessage) {
+        return myMessage + " I've added someting to this!";
+    }
+    }
